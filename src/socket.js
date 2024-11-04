07a6eb84d70005b4d2ec6b01d83968a2b166c55b
YagnikAkbari <yagnik58ppsv@gmail.com>
2024-11-04 14:40:48 +0530

    add env for local development

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const URL = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:5050/";
+const URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/";
 
 export const socket = io(URL, {
   autoConnect: false,
